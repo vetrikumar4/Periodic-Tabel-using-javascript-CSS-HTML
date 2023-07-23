56ca7c08bae5e1aac07b4767069775730bb9f928
vetrikumar4 <46168078+vetrikumar4@users.noreply.github.com>
2023-07-23 06:09:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import boxAttributes from "./index.json" assert { type: "json" };
+const grid = document.createElement("div");
+grid.className = "grid";
+document.body.appendChild(grid);
+boxAttributes.boxAttributes.forEach((boxAttributes) => {
+  const box = document.createElement("div");
+  box.className = `${boxAttributes.color} box animation`;
+  box.textContent = boxAttributes.content;
+  grid.appendChild(box);
+  if (box.textContent.includes(" ")) {
+    box.className = `${boxAttributes.color} box animation`;
+  } else box.className = "box";
+});
